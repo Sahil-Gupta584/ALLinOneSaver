6c8f252b149a58f19edbe1ccf5392c6929ac4aee
Sahil Gupta <guptas3067@gmail.com>
2025-09-16 17:09:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import MainPage from "./components/mainPage";
 
 export default function Home(params) {
-  console.log('asas')
+  console.log(paramsaa)
   return (
     <MainPage defaultChecked={'from main page'} />
   )
