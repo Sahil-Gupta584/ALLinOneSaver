16545fdc37bc4ce84f34db68d6967c2da601f686
Sahil Gupta <guptas3067@gmail.com>
2025-09-27 21:48:51 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client';
 import MainPage from "./components/mainPage";
 
-export default function Home(para) {
-  console.log(pap
+export default function Home(pa {
+  console.log(papP)
   return (
     <MainPage defaultChecked={'from main page'} 
   )
